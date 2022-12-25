70267dedf2e7e7cee770b74683b7a342ec9e05a6
farid-1326 <121006274+farid-1326@users.noreply.github.com>
2022-12-25 22:48:45 +0530

    Delete DAY-6 directory

--- DAY-6/src/com/te/problem2/UserMainCode.java
+++
@@ -1,8 +0,0 @@
-package com.te.problem2;
-
-public class UserMainCode {
-	public static Integer getSubString(String word,String sub) {
-		Integer count = ( word.split(sub, -1).length ) - 1;
-		return count;
-	}
-}
